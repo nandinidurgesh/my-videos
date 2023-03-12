aa0de4e49618f9db75d3b081aeedd1685f5faa7b
nandinidurgesh <nandinidurgesh29@gmail.com>
2023-03-12 17:36:25 +0530

    Styled Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
 import './App.css';
+import Header from "./components/Header";
+import Body from './components/Body';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass,faHouse } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
+  library.add( faMagnifyingGlass,faHouse);
   return (
-    <></>
+    <>
+       <Header></Header>
+       <Body></Body>
+    </>
   );
 }
 
